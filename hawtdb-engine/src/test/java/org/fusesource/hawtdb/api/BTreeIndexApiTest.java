ab68c8c456ae1a91a17175f7dfda1c49265602bd
xuxg <xuxiaoguang@mininglamp.com>
2020-07-15 13:58:02 +0800

    index

--- hawtdb-engine/src/test/java/org/fusesource/hawtdb/api/BTreeIndexApiTest.java
+++ hawtdb-engine/src/test/java/org/fusesource/hawtdb/api/BTreeIndexApiTest.java
@@ -4,7 +4,7 @@ package org.fusesource.hawtdb.api;
  * @author Sergio Bossa
  */
 public class BTreeIndexApiTest extends AbstractApiTest {
-
+    
     @Override
     protected IndexFactory<String, String> getIndexFactory() {
         return new BTreeIndexFactory<String, String>();
--- hawtdb-engine/src/test/java/org/fusesource/hawtdb/api/HashIndexApiTest.java
+++ hawtdb-engine/src/test/java/org/fusesource/hawtdb/api/HashIndexApiTest.java
@@ -4,7 +4,7 @@ package org.fusesource.hawtdb.api;
  * @author Sergio Bossa
  */
 public class HashIndexApiTest extends AbstractApiTest {
-
+    
     @Override
     protected IndexFactory<String, String> getIndexFactory() {
         return new HashIndexFactory<String, String>();
