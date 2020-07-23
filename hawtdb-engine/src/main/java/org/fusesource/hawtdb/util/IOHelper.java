c21cff4d24c44b9d2f2c3c9a3e446a24f7b256ed
xuxg <xuxiaoguang@mininglamp.com>
2020-07-23 19:32:09 +0800

    mvcc

--- hawtdb-engine/src/main/java/org/fusesource/hawtdb/util/IOHelper.java
+++ hawtdb-engine/src/main/java/org/fusesource/hawtdb/util/IOHelper.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,17 +28,18 @@ public final class IOHelper {
     protected static final int MAX_DIR_NAME_LENGTH;
     protected static final int MAX_FILE_NAME_LENGTH;
     private static final int DEFAULT_BUFFER_SIZE = 4096;
+    
     private IOHelper() {
     }
-
+    
     public static String getDefaultDataDirectory() {
         return getDefaultDirectoryPrefix() + "activemq-data";
     }
-
+    
     public static String getDefaultStoreDirectory() {
         return getDefaultDirectoryPrefix() + "amqstore";
     }
-
+    
     /**
      * Allows a system property to be used to overload the default data
      * directory which can be useful for forcing the test cases to use a target/
@@ -51,7 +52,7 @@ public final class IOHelper {
             return "";
         }
     }
-
+    
     /**
      * Converts any string into a string that is safe to use as a file name.
      * The result will only include ascii characters and numbers, and the "-","_", and "." characters.
@@ -72,11 +73,11 @@ public final class IOHelper {
      * The result will only include ascii characters and numbers, and the "-","_", and "." characters.
      *
      * @param name
-     * @param dirSeparators 
-     * @param maxFileLength 
+     * @param dirSeparators
+     * @param maxFileLength
      * @return
      */
-    public static String toFileSystemSafeName(String name,boolean dirSeparators,int maxFileLength) {
+    public static String toFileSystemSafeName(String name, boolean dirSeparators, int maxFileLength) {
         int size = name.length();
         StringBuffer rc = new StringBuffer(size * 2);
         for (int i = 0; i < size; i++) {
@@ -84,9 +85,9 @@ public final class IOHelper {
             boolean valid = c >= 'a' && c <= 'z';
             valid = valid || (c >= 'A' && c <= 'Z');
             valid = valid || (c >= '0' && c <= '9');
-            valid = valid || (c == '_') || (c == '-') || (c == '.') || (c=='#')
-                    ||(dirSeparators && ( (c == '/') || (c == '\\')));
-
+            valid = valid || (c == '_') || (c == '-') || (c == '.') || (c == '#')
+                    || (dirSeparators && ((c == '/') || (c == '\\')));
+            
             if (valid) {
                 rc.append(c);
             } else {
@@ -97,7 +98,7 @@ public final class IOHelper {
         }
         String result = rc.toString();
         if (result.length() > maxFileLength) {
-            result = result.substring(result.length()-maxFileLength,result.length());
+            result = result.substring(result.length() - maxFileLength, result.length());
         }
         return result;
     }
@@ -135,7 +136,7 @@ public final class IOHelper {
                 }
             }
         }
-       
+        
         return result;
     }
     
@@ -164,40 +165,40 @@ public final class IOHelper {
     }
     
     static {
-        MAX_DIR_NAME_LENGTH = Integer.valueOf(System.getProperty("MaximumDirNameLength","200")).intValue();  
-        MAX_FILE_NAME_LENGTH = Integer.valueOf(System.getProperty("MaximumFileNameLength","64")).intValue();             
+        MAX_DIR_NAME_LENGTH = Integer.valueOf(System.getProperty("MaximumDirNameLength", "200")).intValue();
+        MAX_FILE_NAME_LENGTH = Integer.valueOf(System.getProperty("MaximumFileNameLength", "64")).intValue();
     }
-
+    
     
     public static void mkdirs(File dir) throws IOException {
         if (dir.exists()) {
             if (!dir.isDirectory()) {
-                throw new IOException("Failed to create directory '" + dir +"', regular file already existed with that name");
+                throw new IOException("Failed to create directory '" + dir + "', regular file already existed with that name");
             }
             
         } else {
             if (!dir.mkdirs()) {
-                throw new IOException("Failed to create directory '" + dir+"'");
+                throw new IOException("Failed to create directory '" + dir + "'");
             }
         }
     }
     
-	public interface IOStrategy {
-		void sync(FileDescriptor fdo) throws IOException;
-	}	
-	
-	static final IOStrategy IO_STRATEGY = createIOStrategy();
-	
-	private static IOStrategy createIOStrategy() {
-		
-		// On OS X, the fsync system call does not fully flush the hardware buffers.. 
-		// to do that you have to do an fcntl call, and the only way to do that is to
-		// do some JNI.  
-		String os = System.getProperty("os.name");
+    public interface IOStrategy {
+        void sync(FileDescriptor fdo) throws IOException;
+    }
+    
+    static final IOStrategy IO_STRATEGY = createIOStrategy();
+    
+    private static IOStrategy createIOStrategy() {
+        
+        // On OS X, the fsync system call does not fully flush the hardware buffers..
+        // to do that you have to do an fcntl call, and the only way to do that is to
+        // do some JNI.
+//        String os = System.getProperty("os.name");
 //		if( "Mac OS X".equals(os) ) {
 //
 //			// We will gracefully fall back to default JDK file sync behavior
-//			// if the JNA library is not in the path, and we can't set the 
+//			// if the JNA library is not in the path, and we can't set the
 //			// FileDescriptor.fd field accessible.
 //			try {
 //				final Field field = FileDescriptor.class.getDeclaredField("fd");
@@ -205,7 +206,7 @@ public final class IOHelper {
 //				// Try to dynamically load the JNA impl of the CLibrary interface..
 //				final CLibrary lib = getCLibrary();
 //				return new IOStrategy() {
-//					static final int F_FULLFSYNC = 51;	
+//					static final int F_FULLFSYNC = 51;
 //					public void sync(FileDescriptor fd) throws IOException {
 //						try {
 //							int id = field.getInt(fd);
@@ -216,17 +217,17 @@ public final class IOHelper {
 //					}
 //				};
 //			} catch (Throwable ignore) {
-//				// Perhaps we should issue a warning here so folks know that 
+//				// Perhaps we should issue a warning here so folks know that
 //				// the disk syncs are not going to be of very good quality.
 //			}
 //		}
-		
-		return new IOStrategy() {
-			public void sync(FileDescriptor fd) throws IOException {
-				fd.sync();
-			}
-		};
-	}
+        
+        return new IOStrategy() {
+            public void sync(FileDescriptor fd) throws IOException {
+                fd.sync();
+            }
+        };
+    }
 
 //	@SuppressWarnings("unchecked")
 //	public static CLibrary getCLibrary() throws ClassNotFoundException, IllegalAccessException, NoSuchFieldException {
@@ -234,9 +235,9 @@ public final class IOHelper {
 //		final CLibrary lib = (CLibrary) clazz.getField("INSTANCE").get(null);
 //		return lib;
 //	}
-	
-	static public void sync(FileDescriptor fd) throws IOException {
-		IO_STRATEGY.sync(fd);
-	}
-
+    
+    static public void sync(FileDescriptor fd) throws IOException {
+        IO_STRATEGY.sync(fd);
+    }
+    
 }
